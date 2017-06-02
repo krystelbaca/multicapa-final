a3ac8df47e7617d0698f0e498e287f56fb025718
Krystel Baca Salayandia <salayandia.k@gmail.com>
2017-06-02 01:47:28 -0600

    implementando express

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,21 +1,31 @@
 /**
  * Created by krystelbaca on 6/1/17.
  */
-var app = angular.module("app", []);
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-    app.config(function ($routeProvider) {
+var port = process.env.PORT||8080;
 
-        when('/home', {
-            templateUrl: 'home.html',
-            controller: 'HomeCtrl'
-        })
+app.use(bodyParser.json());
 
-        when('/blog', {
-            templateUrl: 'blog.html',
-            controller: 'BlogCtrl'
-        });
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.set('view options', {layout:false});
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/views', express.static(__dirname + '/views'));
 
 
-}
+app.get("/blog", function (req, response) {
+     response.render("blog.ejs");
+});
 
-app.controller("app", home);
+app.get("/oaxaca", function (req, response) {
+    response.render("oaxaca.ejs");
+});
+
+app.listen(port, function(){
+    console.log("Escuchando en el puerto " + port);
+});
