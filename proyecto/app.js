55ec68af8c7e52018794b2feaade91a1b15f6005
Krystel Baca Salayandia <salayandia.k@gmail.com>
2017-06-02 00:19:40 -0600

    index

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,3 +1,21 @@
 /**
  * Created by krystelbaca on 6/1/17.
  */
+var app = angular.module("app", []);
+
+    app.config(function ($routeProvider) {
+
+        when('/home', {
+            templateUrl: 'home.html',
+            controller: 'HomeCtrl'
+        })
+
+        when('/blog', {
+            templateUrl: 'blog.html',
+            controller: 'BlogCtrl'
+        });
+
+
+}
+
+app.controller("app", home);
